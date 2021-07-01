2a104969bdda4687ce471f2ed10725b6bae5908f
Umair Khan <umair.waheed@gmail.com>
2021-07-01 17:19:27 +0500

    Add TopBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import SideMenu from './SideMenu'
 import Main from './Main'
+import SideMenu from './SideMenu'
+import TopBar from './TopBar'
 
 const drawerWidth = 240
 
@@ -103,22 +98,7 @@ export default function App () {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position='absolute' className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge='start'
-            color='inherit'
-            aria-label='open drawer'
-            onClick={handleDrawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography component='h1' variant='h6' color='inherit' noWrap className={classes.title}>
-            ScoopRank
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <TopBar classes={classes} open={open} handleDrawerOpen={handleDrawerOpen} />
       <SideMenu open={open} classes={classes} handleDrawerClose={handleDrawerClose} />
       <Main classes={classes} />
     </div>
