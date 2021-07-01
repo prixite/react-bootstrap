2f27df1bb36b949edf33836055ecd13117ffb4aa
Umair Khan <umair.waheed@gmail.com>
2021-07-01 14:29:42 +0500

    Add Copyright

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,9 @@
 import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
-import Link from '@material-ui/core/Link'
 import Paper from '@material-ui/core/Paper'
+import Copyright from './Copyright'
 import Scoops from './Scoops'
-import Typography from '@material-ui/core/Typography'
-
-function Copyright () {
-  return (
-    <Typography variant='body2' color='textSecondary' align='center'>
-      {'Copyright © '}
-      <Link color='inherit' href='https://scrooprank.com/'>
-        ScoopRank
-      </Link>{' '}
-      {new Date().getFullYear()}
-      .
-    </Typography>
-  )
-}
 
 export default function Main (props) {
   let classes = props.classes
